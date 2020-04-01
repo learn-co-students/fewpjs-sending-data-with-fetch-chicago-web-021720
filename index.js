bf196acbb76d8ba08bb022901fcee638a344b918
GreatDeveloper66 <ashaffe@gmail.com>
2020-04-01 16:18:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your code here
+const header = {
+  "Content-Type":"application/json",
+  "Accept":"application/json"
+};
+const remoteURL = 'http://localhost:3000/users';
+const submitData = (name,email) =>
+  fetch(remoteURL, {
+    method: "POST",
+    headers: header,
+    body: JSON.stringify({name,email})
+  })
+  .then(response => response.json())
+  .then(object => document.querySelector('body').innerHTML = object.id)
+  .catch(error => document.querySelector('body').innerHTML = error.message);
