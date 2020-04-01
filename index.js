39aab0c3793eb78bd174d4aec72b74bd2c70aaf8
mpottebaum <mpottebaum@gmail.com>
2020-03-31 20:12:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email) {
+    let object = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({"name": name, "email": email})
+    }
+    let body = document.querySelector("body")
+    return fetch("http://localhost:3000/users", object)
+        .then(resp => resp.json())
+        .then(function(json) {
+            let id = json.id
+            let p_id = document.createElement("p")
+            p_id.innerHTML = id
+            body.appendChild(p_id)
+        })
+        .catch(function(error) {
+            let p_err = document.createElement("p")
+            p_err.innerHTML = error
+            body.appendChild(p_err)
+        })
+}
