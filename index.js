420bed1b0e535887331c852b00436f6b2940c7b6
ellisandrews <ellisandrews1@gmail.com>
2020-04-01 19:01:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+
+function submitData(userName, userEmail) {
+    
+    const reqData = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    };
+    
+    return fetch('http://localhost:3000/users', reqData)
+             .then(resp => resp.json())
+             .then(json => {
+                 addPToBody(json['id']);
+             })
+             .catch(err => {
+                addPToBody(err.message);
+             });
+}
+
+function addPToBody(text) {
+    const body = document.querySelector('body');
+    const p = document.createElement('p')
+    p.innerHTML = text;
+    body.append(p);
+}
+
